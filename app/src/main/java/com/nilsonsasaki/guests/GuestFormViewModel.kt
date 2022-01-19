bb4ce7e9b5cb5cc83c8c03eca028c9c4b84d3dc7
nilsonsasaki <nilsonsasaki@gmail.com>
2022-01-19 17:15:51 -0300

    created GuestFormViewModel.kt

---
+++ app/src/main/java/com/nilsonsasaki/guests/GuestFormViewModel.kt
@@ -0,0 +1,48 @@
+package com.nilsonsasaki.guests
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.nilsonsasaki.guests.service.models.GuestModel
+import com.nilsonsasaki.guests.service.repository.GuestRepository
+
+enum class Presence { PRESENT, ABSENT, NONE }
+
+class GuestFormViewModel : ViewModel() {
+
+    private val guestRepository = GuestRepository()
+
+    private val _toastText = MutableLiveData<Int>()
+    val toastText: LiveData<Int> = _toastText
+
+    private val _radioButtonStatus = MutableLiveData(Presence.NONE)
+    val radioButtonStatus: LiveData<Presence> = _radioButtonStatus
+
+    fun save(name: String, presence: Presence?) {
+        when (presence) {
+            Presence.PRESENT -> {
+                guestRepository.save(GuestModel(name, "present"))
+                showToast(R.string.saved_sucessfully)
+            }
+            Presence.ABSENT -> {
+                guestRepository.save(GuestModel(name, "absent"))
+                showToast(R.string.saved_sucessfully)
+            }
+            else -> {
+                showToast(R.string.saving_failed)
+            }
+        }
+    }
+
+    fun setRadioButton(presence: Presence) {
+        if (presence != _radioButtonStatus.value) {
+            _radioButtonStatus.value = presence
+        } else {
+            _radioButtonStatus.value = Presence.NONE
+        }
+    }
+
+    private fun showToast(ref: Int) {
+        _toastText.value = ref
+    }
+}
