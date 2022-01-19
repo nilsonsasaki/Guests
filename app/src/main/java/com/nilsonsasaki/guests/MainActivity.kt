c2d3c19bb860ffa504316707f13737f9b93ed66f
nilsonsasaki <nilsonsasaki@gmail.com>
2022-01-19 10:09:05 -0300

    fix renamed template nav ids and removed options menu

--- app/src/main/java/com/nilsonsasaki/guests/MainActivity.kt
+++ app/src/main/java/com/nilsonsasaki/guests/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.nilsonsasaki.guests
 
 import android.os.Bundle
-import android.view.Menu
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -37,17 +36,11 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_all, R.id.nav_presents, R.id.nav_absents
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
