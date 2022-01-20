e1a1976a4ff6ded9c27fcd4963d578e53d9cd3ef
nilsonsasaki <nilsonsasaki@gmail.com>
2022-01-20 15:39:00 -0300

    added save method

--- app/src/main/java/com/nilsonsasaki/guests/service/repository/GuestRepository.kt
+++ app/src/main/java/com/nilsonsasaki/guests/service/repository/GuestRepository.kt
@@ -1,31 +1,63 @@
 package com.nilsonsasaki.guests.service.repository
 
+import android.content.ContentValues
+import android.content.Context
+import com.nilsonsasaki.guests.service.constants.DatabaseConstants
 import com.nilsonsasaki.guests.service.models.GuestModel
+import java.lang.Exception
 
-class GuestRepository {
+class GuestRepository private constructor(context: Context) {
 
-    fun getAll():List<GuestModel>{
+    private var guestDatabaseHelper = GuestDatabaseHelper(context)
+
+    companion object {
+        private lateinit var repository: GuestRepository
+
+        fun getInstance(context: Context): GuestRepository {
+            if (!::repository.isInitialized) {
+                repository = GuestRepository(context)
+            }
+            return repository
+        }
+    }
+
+    fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
     }
-    fun getPresent():List<GuestModel>{
+
+    fun getPresent(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
     }
-    fun getAbsent():List<GuestModel>{
+
+    fun getAbsent(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
     }
 
-    fun save(guest: GuestModel){
+    fun save(guest: GuestModel): Boolean {
+        return try {
+
+            val db = guestDatabaseHelper.writableDatabase
+            val contentValues = ContentValues()
+
+            contentValues.put(DatabaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DatabaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+
+            db.insert(DatabaseConstants.GUEST.TABLE_NAME, null, contentValues)
+
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun update(guest: GuestModel) {
 
     }
 
-    fun update(guest: GuestModel){
-
-    }
-
-    fun delete(guest: GuestModel){
+    fun delete(guest: GuestModel) {
 
     }
 }
