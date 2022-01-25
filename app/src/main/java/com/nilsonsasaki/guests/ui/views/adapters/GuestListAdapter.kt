e266348e6e9708b1a486c3c8f09ea44706218e1f
nilsonsasaki <nilsonsasaki@gmail.com>
2022-01-25 09:40:47 -0300

    created GuestListAdapter

---
+++ app/src/main/java/com/nilsonsasaki/guests/ui/views/adapters/GuestListAdapter.kt
@@ -0,0 +1,42 @@
+package com.nilsonsasaki.guests.ui.views.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.nilsonsasaki.guests.databinding.ListItemBinding
+import com.nilsonsasaki.guests.service.models.GuestModel
+
+class GuestListAdapter(
+    private val list: List<GuestModel>,
+    private val onItemClick: (GuestModel) -> Unit
+) : RecyclerView.Adapter<GuestListAdapter.GuestListViewHolder>() {
+
+    class GuestListViewHolder(binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val name = binding.tvGuestName
+        val presence = binding.tvPresence
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GuestListViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemBinding.inflate(inflater, parent, false)
+        return GuestListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: GuestListViewHolder, position: Int) {
+        holder.name.text = list[position].name
+        holder.presence.text = if (list[position].presence) {
+            "Present"
+        } else {
+            "Absent"
+        }
+        holder.itemView.setOnClickListener{
+            onItemClick(list[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+
+}
