ff17205d33fdad7dcf21f1df924c5aa108b72897
nilsonsasaki <nilsonsasaki@gmail.com>
2022-01-25 09:44:37 -0300

    added list live data

--- app/src/main/java/com/nilsonsasaki/guests/ui/viewmodels/AllGuestsViewModel.kt
+++ app/src/main/java/com/nilsonsasaki/guests/ui/viewmodels/AllGuestsViewModel.kt
@@ -1,13 +1,20 @@
 package com.nilsonsasaki.guests.ui.viewmodels
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.nilsonsasaki.guests.service.models.GuestModel
+import com.nilsonsasaki.guests.service.repository.GuestRepository
 
-class AllGuestsViewModel : ViewModel() {
+class AllGuestsViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is All Guests Fragment"
+    private val repository = GuestRepository.getInstance(application.applicationContext)
+
+    private val _allGuestList = MutableLiveData<List<GuestModel>>()
+    val allGuestsList: LiveData<List<GuestModel>> = _allGuestList
+
+    fun getAllGuests(){
+        _allGuestList.value = repository.getAll()
     }
-    val text: LiveData<String> = _text
 }
